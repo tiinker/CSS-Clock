799c81d8d88f3318d54b705e687160d2e03d023e
tiinker <tiffany.arellano@outlook.com>
2017-12-05 16:03:54 -0800

    Find hour, minute, second and link to line element

--- action.js
+++ action.js
@@ -1,1 +1,22 @@
-console.log("connected");
+var second=document.getElementById("second");
+var minute=document.getElementById("minute");
+var hour=document.getElementById("hour");
+
+var dt = new Date();
+var hr = dt.getHours();
+var min = dt.getMinutes();
+var sec = dt.getSeconds();
+
+console.log(hr, min, sec);
+
+var secDeg = 6*sec;
+var minDeg = 6*min;
+var hrDeg = 30*hr;
+
+setInterval(updateTime);
+
+function updateTime() {
+    second.style.webkitTransform = "rotate("+ secDeg + "deg)";
+    minute.style.webkitTransform = "rotate("+ minDeg + "deg)";
+    hour.style.webkitTransform = "rotate("+ hrDeg + "deg)";
+}

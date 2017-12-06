a5705365a89ce3b9ee45a76f49f4e2b94745f79f
tiinker <tiffany.arellano@outlook.com>
2017-12-05 17:49:17 -0800

    animate hands

--- action.js
+++ action.js
@@ -1,22 +1,18 @@
-var second=document.getElementById("second");
-var minute=document.getElementById("minute");
-var hour=document.getElementById("hour");
 
-var dt = new Date();
-var hr = dt.getHours();
-var min = dt.getMinutes();
-var sec = dt.getSeconds();
-
-console.log(hr, min, sec);
-
-var secDeg = 6*sec;
-var minDeg = 6*min;
-var hrDeg = 30*hr;
-
-setInterval(updateTime);
-
-function updateTime() {
+var time = setInterval(function(){
+    var second=document.getElementById("second");
+    var minute=document.getElementById("minute");
+    var hour=document.getElementById("hour");
+    
+    var dt = new Date();
+    var hr = dt.getHours();
+    var min = dt.getMinutes();
+    var sec = dt.getSeconds();
+    
+    var secDeg = 6 * sec;
+    var minDeg = 6 * min;
+    var hrDeg = 30 * hr;
     second.style.webkitTransform = "rotate("+ secDeg + "deg)";
     minute.style.webkitTransform = "rotate("+ minDeg + "deg)";
     hour.style.webkitTransform = "rotate("+ hrDeg + "deg)";
-}
+}, 1000);
